3245d3e861b3e6514e6979c8deab3ba8d30a329c
Naidu <brahmanaidum93@gmail.com>
2025-02-08 18:53:08 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * 
+    const heading = document.createElement("h2");
+    heading.innerHTML = `This is H2 from JavaScript`;
+    document.body.append(heading);
+ */
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This h1 from React (Child)"),
+    React.createElement("h2", {}, "This h2 from React (Child)"),
+  ]),
+  React.createElement("div", { id: "child02" }, [
+    React.createElement("h1", {}, "This h1 from React (Child02)"),
+    React.createElement("h2", {}, "This h2 from React (Child02)"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+// console.log(typeof root);
+// console.log(heading);
